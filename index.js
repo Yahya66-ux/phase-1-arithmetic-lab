e382734b35180faee7c5dcddf23c8aaa40a604d8
Yahya <yhmoha66@gmail.com>
2023-03-14 23:31:37 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+// generate a random integer greater than 0
+let random = Math.floor(Math.random() * 100) + 1;
+
+// calculate the remainder of dividing num3 by num4 to get 4
+let num3 = 28;
+let num4 = 6;
+let mod = num3 % num4;
+
+// find the highest number in a set
+let numbers = [5, 10, 20, 15];
+let max = Math.max(...numbers);
